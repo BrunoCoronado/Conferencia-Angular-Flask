f669646fee97eecf76477975e81f14cbc6ae10ff
Bruno Coronado <bcoronado.morales@gmail.com>
2021-10-07 18:54:33 -0600

    Funcionalidad API

--- API/app.py
+++ API/app.py
@@ -1,40 +1,105 @@
 #Imports
 from flask import Flask, request, jsonify
 
+#Variables globales
+usuarios = [
+        {
+            'usuario': 'admin',
+            'password': '123456',
+            'rol': 1,
+        },
+        {
+            'usuario': 'cliente',
+            'password': '123456',
+            'rol': 2,
+        },
+    ]
+productos = []
+
 #Instancia de flask con el nombre de la variable especial __name__ del modulo
 app = Flask(__name__)
 
 @app.route('/')
 def status():
     return jsonify(
+        usuarios = usuarios,
         mensaje = 'Servidor Corriendo',
         status = 200
     )
 
-#Autenticación de Usuarios
+"""Autenticación de Usuarios"""
 @app.route('/login', methods=['POST'])
 def login():
-    return jsonify(
-        mensaje = 'POST login',
-        status = 200
-    )
+    try:
+        #Buscamos el usuario por nombre dentro del arreglo, si no existe retorna None
+        usuario = next((u for u in usuarios if u["usuario"] == request.json['usuario']), None)
 
-#Ingreso de Productos
+        #Si el usuario encontrado es distinto de None y la contraseña coincide
+        if usuario is not None and usuario["password"] == request.json['password']:
+            #retornamos la metadata del usuario y un estado 200 (ok)
+            return jsonify(
+                usuario = usuario['usuario'],
+                rol = usuario['rol'],
+                mensaje = 'Bienvenido al Sistema!',
+                status = 200
+            )
+
+        #retornamos estado 404 (not found) y un mensaje de error
+        return jsonify(
+            mensaje = 'Usuario o contraseña inválidas',
+            status = 404
+        )
+    except Exception as e:
+        print(e)
+        #retornamos estado 400 (Bad request) y un mensaje de error
+        return jsonify(
+            mensaje = 'Ocurrio un error desconocido',
+            status = 400
+        )
+
+"""Ingreso de Productos"""
 @app.route('/producto', methods=['POST'])
 def producto():
-    return jsonify(
-        mensaje = 'POST producto',
-        status = 200
-    )
-    
-#Obtener Productos Registrados    
-@app.route('/productos', methods=['GET'])
-def productos():
-    return jsonify(
-        mensaje = 'GET productos',
-        status = 200
-    )
+    try:
+        #Agregamos un producto al arreglo con los datos de la petición
+        productos.append(
+                {
+                    "nombre": request.json['nombre'],
+                    "precio": request.json['precio'],
+                    "imagen": request.json['imagen'],
+                }
+            )
 
-#Entrypoint cuando es ejecutado app.py
+        #retornamos estado 200 (ok) y un mensaje
+        return jsonify(
+            mensaje = 'Producto Creado con éxito',
+            status = 200
+        )
+    except Exception as e:
+        print(e)
+        #retornamos estado 400 (Bad request) y un mensaje de error
+        return jsonify(
+            mensaje = 'Ocurrio un error desconocido',
+            status = 400
+        )
+
+"""Obtener Productos Registrados"""
+@app.route('/productos', methods=['GET'])
+def obtenerProductos():
+    try:
+        #Retornamos todos los productos
+        return jsonify(
+            productos = productos,
+            status = 200
+        )
+    except Exception as e:
+        print(e)
+        #retornamos estado 400 (Bad request) y un mensaje de error
+        return jsonify(
+            mensaje = 'Ocurrio un error desconocido',
+            status = 400
+        )
+
+"""Entrypoint cuando es ejecutado app.py"""
 if __name__ == "__main__":
     app.run(debug=True)
